d98d1e3e0d21b635bc45f54fe44a1b026c996b6e
mmo <thetechpomelo@gmail.com>
2022-04-27 11:57:03 +0200

    add connect to mm

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,31 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import { useWeb3React } from '@web3-react/core'
+
+
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+
+// connector
+const injected = new InjectedConnector(
+  { supportedChainIds: [1, 3, 4, 5, 42] } //suported networks 
+)
+
 
 export default function Home() {
+  const { active, account, chainId, activate, deactivate } = useWeb3React();
+  console.log(account)
+  console.log(chainId)
+  console.log(active)
+
   return (
-    <div className={styles.container}>
-         asdfasf
+    <div>
+      {active ? (
+        <div>Connected:{account}
+          <button onClick={deactivate}>Disconnect</button>
+        </div>) :
+        (<button onClick={() => { activate(injected) }}>Connect Metamask</button>)
+      }
     </div>
   )
 }
