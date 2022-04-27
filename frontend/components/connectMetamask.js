c60ba65389be014b4a5a65e4a3a5e13fc7278299
mmo <thetechpomelo@gmail.com>
2022-04-27 20:54:23 +0200

    add mm

--- frontend/components/connectMetamask.js
+++ frontend/components/connectMetamask.js
@@ -3,15 +3,28 @@ import { injected } from '../connectors';
 
 //stiling
 import { Button } from '@mui/material';
+
 export default function ConnectMetamask() {
-    const { active, account, chainId, activate, deactivate } = useWeb3React();
+    const { active, library, connector, account, chainId, activate, deactivate } = useWeb3React();
     console.log(account)
     console.log(chainId)
     console.log(active)
+    const get_balance = () => {
+        return async() =>{
+            const balance = await library.getBalance("0x4aAded56bd7c69861E8654719195fCA9C670EB45");
+            return balance
+        }
+    }
+        if (active) {
+            console.log("lib is", library)
+            console.log("dai balance:", get_balance())
+        }
+    
     // later add change to correct network button or alert to change it manually
     //injected.supportedChainIds.includes(chainId) ?
     // console.log("correct network") :
     // console.log("wrong network")
+
     return (
         <div>
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,13 @@
 import { Web3ReactProvider } from "@web3-react/core"
 import Home from ".";
 import { Web3Provider } from "@ethersproject/providers"
-
+import { injected } from "../connectors";
 
 
 function getLibrary(provider) {
   return new Web3Provider(provider);
 }
+
 
 function MyApp({ Component, pageProps }) {
   return (
