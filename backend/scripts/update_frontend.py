6ca61855f6d59c1f0b9eaa5bbf8350a25d03b7bb
Mario M. Orozco <41654186+cito-lito@users.noreply.github.com>
2022-06-27 18:43:38 +0200

    Update update_frontend.py

--- backend/scripts/update_frontend.py
+++ backend/scripts/update_frontend.py
@@ -1,17 +1,19 @@
 import os, shutil, yaml, json
 
+#### copy useful data (configs, address, ABIs..) to the frontend folder ###
+
 new_frontend_path = "/home/mmo/Desktop/testDapp/test_dapp/brownie_build"
 new_config_path = "/home/mmo/Desktop/testDapp/frontend/brownie-config.json"
 def update_frontend():
     # Send the build folder
     copy_folders("./build", new_frontend_path)
 
-    # Send the front end the config in JSON
+    # Send to the front end the config in JSON
     with open("brownie-config.yaml", "r") as brownie_config:
         config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
         with open(new_config_path, "w") as brownie_config_json:
             json.dump(config_dict, brownie_config_json)
-    print("Front end updated!")
+    print("done")
 
 
 def copy_folders(src, dest):
